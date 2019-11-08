ef2ab6e8952b45547bb096d22594be25efbd455e
huangyuzhen <huangyuzhen@earch>
2019-11-08 13:17:47 +0800

    水桶问题

--- 11/11.py
+++ 11/11.py
@@ -1,27 +1,40 @@
 from typing import List
 
 class Solution:
+    def moveRight(self, height, i, j):
+        # 从i向右移动指针，找到比i更高的木板位置
+        k = i+1
+        while k < j and height[k] < height[i]:
+           k += 1
+        return k
+
+    def moveLeft(self, height, i, j):
+        # 从j向左移动指针，找到比j更高的木板位置
+        k = j-1
+        while k > i and height[k] < height[j]:
+            k -= 1
+        return k
+
     def maxArea(self, height: List[int]) -> int:
         length = len(height)
         maxV = 0
-        i = 0
-        j = length-1
+        i, j = 0, length-1
 
         while i<j:
             h = min(height[i], height[j])
             v = h * (j-i)
-            print((i,j) ,v)
+            # print((i,j) ,v)
             if v > maxV:
                 maxV = v
 
             if height[i] < height[j]:
-                i += 1
+                i = self.moveRight(height, i, j)
             elif height[i] > height[j]:
-                j -= 1
+                j = self.moveLeft(height, i, j)
             else:
                 # 高相等时,移动更快
-                i += 1
-                j -= 1
+                i = self.moveRight(height, i, j)
+                j = self.moveLeft(height, i, j)
 
         return maxV
 
