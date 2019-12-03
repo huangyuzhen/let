363acfb82d59039fdce5ec4b9e35d5ba22cbf41a
huangyuzhen <huangyuzhen@earch>
2019-12-03 22:26:56 +0800

    反转

--- 92/b.py
+++ 92/b.py
@@ -8,15 +8,15 @@ class ListNode(object):
         self.next = None
 
 def reverseN(head, n):
-    def reverseNt(head, N, n):
+    def reverseNt(head, n):
         tail = head.next
         head.next = None
         if not tail or n == 1: return head, tail
-        last, part = reverseNt(tail, N, n-1)
+        last, part = reverseNt(tail, n-1)
         tail.next = head
         return last, part
 
-    last, part = reverseNt(head, n, n)
+    last, part = reverseNt(head, n)
     head.next = part
     return last
 
