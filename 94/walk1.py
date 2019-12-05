eb050aad9ad2d0d82956ca796fe46647ba829888
huangyuzhen <huangyuzhen@earch>
2019-12-05 09:02:52 +0800

    前序遍历

---
+++ 94/walk1.py
@@ -0,0 +1,74 @@
+# Definition for a binary tree node.
+class TreeNode(object):
+    def __init__(self, x):
+        self.val = x
+        self.left = None
+        self.right = None
+
+class Solution(object):
+    def preorderHelper(self, root, result):
+        if root:
+            result.append(root.val)
+            self.preorderHelper(root.left, result)
+            self.preorderHelper(root.right, result)
+
+    def preorderTraversal(self, root):
+        result = []
+        self.preorderHelper(root, result)
+        return result
+
+    def preorderTraversal1(self, root):
+        result = []
+        stack  = []
+        node = root
+        while node or stack:
+            if node:
+                result.append(node.val)
+                stack.append(node)
+                node = node.left
+            else:
+                node = stack.pop()
+                node = node.right
+        return result
+
+    def preorderTraversal2(self, root):
+        result = []
+        stack = [root]
+
+        while stack:
+            node = stack.pop()
+            if hasattr(node, 'inorderFlag') and node.inorderFlag:
+                result.append(node.val)
+            else:
+                if node.right:
+                    stack.append(node.right)
+
+                if node.left:
+                    stack.append(node.left)
+
+                node.inorderFlag = True
+                stack.append(node)
+
+        return result
+
+
+
+def initData():
+    root = TreeNode(4)
+    root.left = TreeNode(2)
+    root.right = TreeNode(6)
+
+    root.left.left = TreeNode(1)
+    root.left.right = TreeNode(3)
+
+    root.right.left = TreeNode(5)
+    root.right.right = TreeNode(7)
+    return root
+
+
+root = initData()
+solution = Solution()
+print(solution.preorderTraversal(root))
+print(solution.preorderTraversal1(root))
+print(solution.preorderTraversal2(root))
+
