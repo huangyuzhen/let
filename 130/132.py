23cae02fe7e1056e5e262489f9d422f79e41a2c5
huangyuzhen <huangyuzhen@earch>
2019-12-21 18:04:41 +0800

    132 分割回文串, 用了2次动态规划

---
+++ 130/132.py
@@ -0,0 +1,37 @@
+class Solution(object):
+    def __init__(self):
+        self.memTable = {}
+
+    def minCut(self, s):
+        """
+        :type s: str
+        :rtype: int
+        """
+        length = len(s)
+        if length <= 1: return 0
+        if s == s[::-1]: return 0
+
+        if self.memTable.get(s) != None:
+            return self.memTable[s]
+
+        bestV = 99999
+        for i in range(1, length):
+            first = s[:i]
+            if first == first[::-1]:
+                minV  = self.minCut(s[i:])
+                bestV = min(bestV, minV + 1)
+
+        self.memTable[s] = bestV
+
+        return bestV
+
+
+s = "aab"
+s = "fifgbeajcacehiicccfecbfhhgfiiecdcjjffbghdidbhbdbfbfjccgbbdcjheccfbhafehieabbdfeigbiaggchaeghaijfbjhi"
+
+sol = Solution()
+x = sol.minCut(s)
+print(x)
+
+for k in sol.memTable:
+    print(k, sol.memTable[k])
---
+++ 130/132b.py
@@ -0,0 +1,40 @@
+class Solution:
+    def minCut(self, s: str) -> int:
+        length = len(s)
+        if length <= 1: return 0
+
+        dp = [[0] * length for _ in range(length)]
+        for i in range(length): dp[i][i] = 1
+        for j in range(1, length):
+            for i in range(j):
+                dp[i][j] = 0
+                if s[i] == s[j]:
+                    if i+2 <= j:
+                        dp[i][j] = dp[i+1][j-1]
+                    else:
+                        dp[i][j] = 1
+
+        resultDp = [0] * length
+        for j in range(1, length):
+            if dp[0][j]:
+                resultDp[j] = 0
+                continue
+            # 枚举分割点
+            minV = 9999999
+            for i in range(j):
+                if dp[i+1][j]:
+                    minV = min(resultDp[i], minV)
+
+            resultDp[j] = minV + 1
+
+        return resultDp[-1]
+
+
+s = "aab"
+s = "fifgbeajcacehiicccfecbfhhgfiiecdcjjffbghdidbhbdbfbfjccgbbdcjheccfbhafehieabbdfeigbiaggchaeghaijfbjhi"
+
+# s = "fifgbbga"
+
+sol = Solution()
+x = sol.minCut(s)
+print(x)
