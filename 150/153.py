8c15b198a6ec3a03f6a68b76462d2befc0dc0179
huangyuzhen <huangyuzhen@earch>
2020-01-04 22:24:29 +0800

    153. 寻找旋转排序数组中的最小值

---
+++ 150/153.py
@@ -0,0 +1,31 @@
+class Solution(object):
+    def findMin(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+
+        length = len(nums)
+        if length == 0: return 0
+        if length == 1: return nums[0]
+
+        rotateIndex = 0
+        if nums[0] > nums[-1]:
+            lb, ub = 0, length - 1
+            while lb < ub:
+                mid = (lb + ub) // 2
+                if nums[mid] > nums[rotateIndex]:
+                    rotateIndex = mid
+                    lb = mid+1
+                else:
+                    ub = mid
+            rotateIndex += 1
+
+        return nums[rotateIndex]
+
+
+
+nums = [5,6,7,8,9,10,11,1,2,3,4]
+sol = Solution()
+x = sol.findMin(nums)
+print(x)
