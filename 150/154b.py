01d8162fbc5d4e7ac30cf5bd10d666284350cdc6
huangyuzhen <huangyuzhen@earch>
2020-01-06 20:53:41 +0800

    154 最小栈

---
+++ 150/154b.py
@@ -0,0 +1,41 @@
+class MinStack:
+
+    # 辅助栈和数据栈同步
+    # 思路简单不容易出错
+
+    def __init__(self):
+        # 数据栈
+        self.data = []
+        # 辅助栈
+        self.helper = []
+
+    def push(self, x):
+        self.data.append(x)
+        if len(self.helper) == 0 or x <= self.helper[-1]:
+            self.helper.append(x)
+        else:
+            self.helper.append(self.helper[-1])
+
+    def pop(self):
+        if self.data:
+            self.helper.pop()
+            return self.data.pop()
+
+    def top(self):
+        if self.data:
+            return self.data[-1]
+
+    def getMin(self):
+        if self.helper:
+            return self.helper[-1]
+
+
+
+minStack = MinStack()
+minStack.push(-2)
+minStack.push(0)
+minStack.push(-3)
+print(minStack.getMin())
+minStack.pop()
+print(minStack.top())
+print(minStack.getMin())
